21c5bd299d1b041ab27fee128edef6be7365c155
Park June Chul <pjc0247@naver.com>
2017-01-16 15:29:31 +0900

    init

---
+++ S3AAD/RemoteDocument.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using JsonFx.Json;
+
+namespace S3AAD
+{
+	[Serializable]
+	public class RemoteDocument
+	{
+		public string bucketName { get; private set; }
+		public string key { get; private set; }
+		public Dictionary<string, object> data { get; private set; }
+
+		public object this[string key]
+		{
+			get
+			{
+				return data[key];
+			}
+			set
+			{
+				data[key] = value;
+			}
+		}
+
+		internal RemoteDocument(string bucketName, string key)
+		{
+			this.bucketName = bucketName;
+			this.key = key;
+			this.data = new Dictionary<string, object>();
+		}
+		internal RemoteDocument(string json)
+		{
+			Console.WriteLine(json);
+			var reader = new JsonReader();
+			var _this = reader.Read<Dictionary<string, object>>(json);
+
+			Console.WriteLine(_this["data"]);
+			bucketName = ((string)_this["bucketName"]);
+			key = ((string)_this["key"]);
+			data = new Dictionary<string, object>((System.Dynamic.ExpandoObject)_this["data"]);
+		}
+
+		public void Update()
+		{
+			S3DB.SaveDocument(bucketName, key, this);
+		}
+		public string AsJson()
+		{
+			var writer = new JsonWriter();
+			return writer.Write(new
+			{
+				bucketName = bucketName,
+				key = key,
+				data = data
+			});
+		}
+	}
+}
---
+++ S3AAD/S3DB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3AAD
+{
+	public class S3DB
+	{
+		private static AmazonS3Client client;
+
+		public static void Init(string accessKey, string accessSecret, string regionName)
+		{
+			client = new AmazonS3Client(
+				accessKey, accessSecret, RegionEndpoint.GetBySystemName(regionName));
+		}
+
+		internal static bool SaveDocument(string bucketName, string key, RemoteDocument document)
+		{
+			if (client == null)
+				throw new InvalidOperationException("not initialized");
+
+			var resp = client.PutObject(new PutObjectRequest()
+			{
+				BucketName = bucketName,
+				Key = key,
+				ContentBody = document.AsJson()
+			});
+
+			Console.WriteLine(resp.HttpStatusCode);
+			return resp.HttpStatusCode == System.Net.HttpStatusCode.OK;
+		}
+
+		public static RemoteDocument CreateDocument(string bucketName, string key)
+		{
+			if (client == null)
+				throw new InvalidOperationException("not initialized");
+
+			try
+			{
+				var document = new RemoteDocument(bucketName, key);
+
+				SaveDocument(bucketName, key, document);
+
+				return document;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				return null;
+			}
+		}
+		public static RemoteDocument FindDocument(string bucketName, string key)
+		{
+			if (client == null)
+				throw new InvalidOperationException("not initialized");
+
+			try
+			{
+				var resp = client.GetObject(bucketName, key);
+				var reader = new StreamReader(resp.ResponseStream);
+
+				return new RemoteDocument(reader.ReadToEnd());
+			}
+			catch (JsonFx.Serialization.DeserializationException e)
+			{
+				throw new InvalidOperationException(bucketName + "::" + key + " is not a S3AAD document.");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				return null;
+			}
+		}
+	}
+}
