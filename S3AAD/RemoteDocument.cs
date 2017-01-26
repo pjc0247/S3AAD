793a3cbfe89f45477846f8b394caee834bedc609
pmnxis <pmnxis@gmail.com>
2017-01-26 12:14:57 +0900

    update

--- S3AAD/RemoteDocument.cs
+++ S3AAD/RemoteDocument.cs
@@ -1,60 +1,90 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 
 using JsonFx.Json;
 
 namespace S3AAD
 {
-	[Serializable]
-	public class RemoteDocument
-	{
-		public string bucketName { get; private set; }
-		public string key { get; private set; }
-		public Dictionary<string, object> data { get; private set; }
-
+    [Serializable]
+    public class RemoteDocument : IDictionary<string, object>
+    {
+        public string bucketName { get; private set; }
+        public string key { get; private set; }
+        public Dictionary<string, object> data { get; private set; }
+        
         public bool publicRead;
 
-		public object this[string key]
-		{
-			get
-			{
-				return data[key];
-			}
-			set
-			{
-				data[key] = value;
-			}
-		}
+        public object this[string key]
+        {
+            get
+            {
+                return data[key];
+            }
+            set
+            {
+                data[key] = value;
+            }
+        }
 
-		internal RemoteDocument(string bucketName, string key)
-		{
-			this.bucketName = bucketName;
-			this.key = key;
-			this.data = new Dictionary<string, object>();
-		}
-		internal RemoteDocument(string json)
-		{
-			var reader = new JsonReader();
-			var _this = reader.Read<Dictionary<string, object>>(json);
+        internal RemoteDocument(string bucketName, string key)
+        {
+            this.bucketName = bucketName;
+            this.key = key;
+            this.data = new Dictionary<string, object>();
+        }
+        internal RemoteDocument(string json)
+        {
+            var reader = new JsonReader();
+            var _this = reader.Read<Dictionary<string, object>>(json);
 
-			bucketName = ((string)_this["bucketName"]);
-			key = ((string)_this["key"]);
-			data = new Dictionary<string, object>((System.Dynamic.ExpandoObject)_this["data"]);
-		}
+            bucketName = ((string)_this["bucketName"]);
+            key = ((string)_this["key"]);
+            data = new Dictionary<string, object>((System.Dynamic.ExpandoObject)_this["data"]);
+        }
 
-		public void Update()
-		{
-			S3DB.SaveDocument(bucketName, key, this, publicRead);
-		}
-		public string AsJson()
-		{
-			var writer = new JsonWriter();
-			return writer.Write(new
-			{
-				bucketName = bucketName,
-				key = key,
-				data = data
-			});
-		}
-	}
+        public void Update()
+        {
+            S3DB.SaveDocument(bucketName, key, this, publicRead);
+        }
+
+        public string AsJson()
+        {
+            var writer = new JsonWriter();
+            return writer.Write(new
+            {
+                bucketName = bucketName,
+                key = key,
+                data = data
+            });
+        }
+
+        #region IDictionary<string, obect>
+        public ICollection<string> Keys => data.Keys;
+        public ICollection<object> Values => data.Values;
+        public int Count => data.Count;
+        public bool IsReadOnly => false;
+
+        public bool ContainsKey(string key) => data.ContainsKey(key);
+        public void Add(string key, object value) => data.Add(key, value);
+        bool Remove(string key) => data.Remove(key);
+        bool IDictionary<string, object>.Remove(string key) => data.Remove(key);
+        public bool TryGetValue(string key, out object value) => data.TryGetValue(key, out value);
+        public void Add(KeyValuePair<string, object> item) => data.Add(item.Key, item.Value);
+        public void Clear() => data.Clear();
+        public bool Contains(KeyValuePair<string, object> item) => data.Any(x => x.Key == item.Key && x.Value == item.Value);
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => data.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => data.GetEnumerator();
+
+        public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Remove(KeyValuePair<string, object> item)
+        {
+            throw new NotImplementedException("use `Remove(string key)` instaed");
+        }
+        #region
+    }
 }
--- S3AAD/S3DB.cs
+++ S3AAD/S3DB.cs
@@ -7,76 +7,76 @@ using Amazon.S3.Model;
 
 namespace S3AAD
 {
-	public class S3DB
-	{
-		private static AmazonS3Client client;
+    public class S3DB
+    {
+        private static AmazonS3Client client;
 
-		public static void Init(string accessKey, string accessSecret, string regionName)
-		{
-			client = new AmazonS3Client(
-				accessKey, accessSecret, RegionEndpoint.GetBySystemName(regionName));
-		}
+        public static void Init(string accessKey, string accessSecret, string regionName)
+        {
+            client = new AmazonS3Client(
+                accessKey, accessSecret, RegionEndpoint.GetBySystemName(regionName));
+        }
 
-		internal static bool SaveDocument(string bucketName, string key, RemoteDocument document, bool publicRead = false)
-		{
-			if (client == null)
-				throw new InvalidOperationException("not initialized");
+        internal static bool SaveDocument(string bucketName, string key, RemoteDocument document, bool publicRead = false)
+        {
+            if (client == null)
+                throw new InvalidOperationException("not initialized");
 
-			var resp = client.PutObject(new PutObjectRequest()
-			{
-				BucketName = bucketName,
-				Key = key,
-				ContentBody = document.AsJson(),
+            var resp = client.PutObject(new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = key,
+                ContentBody = document.AsJson(),
 
                 CannedACL = publicRead ? S3CannedACL.PublicRead : S3CannedACL.NoACL
-			});
+            });
 
-			return resp.HttpStatusCode == System.Net.HttpStatusCode.OK;
-		}
+            return resp.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
 
-		public static RemoteDocument CreateDocument(string bucketName, string key, bool publicRead = false)
-		{
-			if (client == null)
-				throw new InvalidOperationException("not initialized");
+        public static RemoteDocument CreateDocument(string bucketName, string key, bool publicRead = false)
+        {
+            if (client == null)
+                throw new InvalidOperationException("not initialized");
 
-			try
-			{
+            try
+            {
                 var document = new RemoteDocument(bucketName, key)
                 {
                     publicRead = publicRead
                 };
 
-				SaveDocument(bucketName, key, document);
+                SaveDocument(bucketName, key, document);
 
-				return document;
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-				return null;
-			}
-		}
-		public static RemoteDocument FindDocument(string bucketName, string key)
-		{
-			if (client == null)
-				throw new InvalidOperationException("not initialized");
+                return document;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+        public static RemoteDocument FindDocument(string bucketName, string key)
+        {
+            if (client == null)
+                throw new InvalidOperationException("not initialized");
 
-			try
-			{ 
-				var resp = client.GetObject(bucketName, key);
-				var reader = new StreamReader(resp.ResponseStream);
+            try
+            { 
+                var resp = client.GetObject(bucketName, key);
+                var reader = new StreamReader(resp.ResponseStream);
 
-				return new RemoteDocument(reader.ReadToEnd());
-			}
-			catch (JsonFx.Serialization.DeserializationException e)
-			{
-				throw new InvalidOperationException(bucketName + "::" + key + " is not a S3AAD document.");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-				return null;
-			}
-		}
-	}
+                return new RemoteDocument(reader.ReadToEnd());
+            }
+            catch (JsonFx.Serialization.DeserializationException e)
+            {
+                throw new InvalidOperationException(bucketName + "::" + key + " is not a S3AAD document.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
 }
